f3aea3cc2fc208592795554d8243c583f6aff954
CrunchyHotDogs <Kcross119@yahoo.ca>
2015-03-05 15:02:19 -0500

    Added error checking for the image insert.

--- web/Javascript/createBoard.js
+++ web/Javascript/createBoard.js
@@ -10,6 +10,41 @@ $(document).ready(function () {
     initializeElements();
 });
 
+function readImage(file) {
+    var reader = new FileReader();
+    var image  = new Image();
+
+    reader.readAsDataURL(file);  
+    reader.onload = function(_file) {
+        image.src = _file.target.result;
+        image.onload = function() {
+            var errorMessage = "";
+        
+            if (this.width !== 800) {
+                errorMessage += "Picture must be 800px wide.\n";
+            }
+            if (this.height !== 800) {
+                errorMessage += "Picture must be 800px high.\n";
+            }
+            if (file.type !== "image/jpeg") {
+                errorMessage += "Picture must be of the jpg/jpeg format.\n";
+            }
+            
+            if (errorMessage.length > 0) {
+                $('#imageNextButton').prop('disabled', true);
+                alert(errorMessage);
+            }
+            else {
+                $('#imageNextButton').prop('disabled', false);
+                alert("Image is good.");
+            }
+        };
+        image.onerror = function() {
+            alert('Invalid file type: '+ file.type);
+        };      
+    };
+}
+
 function showNextTab(nextForm, nextTab) {
     $('#forms').find("div[id^='form']").each(function (index, element) {
        $(this).hide();
@@ -26,6 +61,20 @@ function showNextTab(nextForm, nextTab) {
 }
 
 function initializeElements() {
+    $("#myFile").change(function (e) {
+        if (this.disabled) {
+            return alert('File upload not supported!');
+        }
+        
+        var F = this.files;
+        
+        if(F && F[0]) {
+            for (var i = 0; i < F.length; i++) { 
+                readImage( F[i] );
+            } 
+        }
+    });
+    
     $('#propertyNextButton').click(function () {
         showNextTab('formChance', 'chanceTab');
     });
