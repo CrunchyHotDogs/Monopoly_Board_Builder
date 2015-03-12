5436585d92137176666b487f410f2028b968a5fa
CrunchyHotDogs <Kcross119@yahoo.ca>
2015-03-12 12:53:42 -0400

    Added a check for the database connection so if it fails to connect to the server, it will try and connect to the localhost.

--- src/java/credentials/Credentials.java
+++ src/java/credentials/Credentials.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 public class Credentials {
     public static Connection getConnection() {
         Connection connection = null;
+        boolean FLAG = true;
         
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -19,8 +20,23 @@ public class Credentials {
             String pass = "";
             connection = DriverManager.getConnection(jdbc, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
+            FLAG = false;
             Logger.getLogger(Credentials.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        //If the program can't get a connection to the server, try getting a connection on th localhost.
+        if (FLAG == false) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                String jdbc = "jdbc:mysql://localhost/mbb";
+                String user = "root";
+                String pass = "";
+                connection = DriverManager.getConnection(jdbc, user, pass);
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Credentials.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
         return connection;
     }
 }
