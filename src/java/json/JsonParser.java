90f46d007e7e7b9c735965050f1c8a43c4b6338a
CrunchyHotDogs <Kcross119@yahoo.ca>
2015-03-18 13:17:26 -0400

    Completed the JsonParser function that exports a whole boards json.

--- src/java/json/JsonParser.java
+++ src/java/json/JsonParser.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
 /**
@@ -14,8 +15,50 @@ import javax.json.JsonObjectBuilder;
  * @author Kyle
  */
 public class JsonParser {
-    public static String getSpecificBoardJson(ResultSet board, ResultSet property, ResultSet chance, ResultSet communty) {
-        return "";
+    public static String getSpecificBoardJson(ResultSet board, ResultSet property, ResultSet chance, ResultSet communty) throws SQLException {
+        JsonArrayBuilder jsonArray = Json.createArrayBuilder();
+        JsonObjectBuilder singleJson = Json.createObjectBuilder();
+        JsonObjectBuilder fullJson = Json.createObjectBuilder();
+        
+        while (property.next()) {
+            singleJson.add("name", property.getString("name"))
+                    .add("tax", property.getString("tax"))
+                    .add("house", property.getString("house_cost"))
+                    .add("cost", property.getString("cost"))
+                    .add("type", property.getString("type"));
+            jsonArray.add(singleJson);
+        }
+        
+        fullJson.add("property", jsonArray);
+        
+        while (chance.next()) {
+            singleJson.add("name", chance.getString("name"))
+                    .add("desctiption", chance.getString("description"))
+                    .add("type", chance.getString("type"));
+            jsonArray.add(singleJson);
+        }
+        
+        fullJson.add("chanceCard", jsonArray);
+        
+        while (communty.next()) {
+            singleJson.add("name", communty.getString("name"))
+                    .add("desctiption", communty.getString("description"))
+                    .add("type", communty.getString("type"));
+            jsonArray.add(singleJson);
+        }
+        
+        fullJson.add("communityChest", jsonArray);
+        
+        while (board.next()) {
+            singleJson.add("name", board.getString("board_name"))
+                    .add("url", board.getString("image_url"));
+            jsonArray.add(singleJson);
+        }
+        
+        fullJson.add("board", jsonArray);
+        
+        JsonObject completeJson = fullJson.build();
+        return completeJson.toString();
     }
     
     public static String getAllBoards(ResultSet info) throws SQLException {
